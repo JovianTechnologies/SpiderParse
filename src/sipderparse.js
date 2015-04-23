d2d48c4dd3d3b2a4fab8131afdd324a84bcae301
Eric Smallwood <smallwood.eric@ymail.com>
2015-04-23 07:03:01 -0500

    New project started (moved from ScrubJay.js)

---
+++ src/sipderparse.js
@@ -0,0 +1,129 @@
+/**
+ * Created by Eric on 4/23/2015.
+ */
+(function(window){
+    window.SpiderParse = {
+        parse: function(htmlString){
+            var self = this;
+
+            var Child = function(){
+                this.attrs = [];
+                this.childNodes = [];
+                this.innerHTML = "";
+                this.name = "";
+                this.outerHTML = "";
+            };
+
+            /**
+             * Get all attributes and thier values from a tag
+             * @param tagString
+             * @param attrsList
+             */
+            function getTagAttributes(tagString, attrsList){
+                var delineatorsRegex = /\s|['"]|\s*\/?>/;
+                var attrLocation = tagString.search(delineatorsRegex);
+                if(tagString.search(/\s*\/?>$/) > 0 && attrLocation >= 0){
+                    tagString = tagString.substring(attrLocation + 1).trim();
+                    var attr = tagString.substring(0, tagString.search(delineatorsRegex));
+                    if(!attr.search(/\s*/)){
+                        //parse attribute name
+                        var assignmentOperatorLocation = attr.indexOf("=");
+                        var name = assignmentOperatorLocation >=0  ? attr.substring(0,assignmentOperatorLocation).trim() : attr;
+
+                        //parse attribute value
+                        tagString = tagString.substring(assignmentOperatorLocation + 1);
+                        var valueRegex = /^[^'"]*[\s\/>]|^["'].*["'](\s|\/|>)?/;
+                        var value = assignmentOperatorLocation < 0 ? null : tagString.match(valueRegex)[0];
+                        var trimmedValue = value == null ? null : value.trim();
+
+                        if(trimmedValue != null) {
+                            //remove any extra "'s
+                            trimmedValue = trimmedValue.replace(/^"(.*)"$/, '$1');
+
+                            //if last character is / or > remove it
+                            if (trimmedValue.lastIndexOf("/") == trimmedValue.length - 2)
+                                trimmedValue = trimmedValue.substring(0, trimmedValue.lastIndexOf("/"));
+                            else if (trimmedValue.lastIndexOf(">") == trimmedValue.length - 1)
+                                trimmedValue = trimmedValue.substring(0, trimmedValue.lastIndexOf(">"));
+                        }
+
+                        attrsList.push({name: name, value: trimmedValue});
+
+                        if(assignmentOperatorLocation < 0){
+                            getTagAttributes(tagString.substring(tagString.indexOf(attr) + attr.length), attrsList);
+                        }else {
+                            getTagAttributes(tagString.substring(tagString.indexOf(trimmedValue) + trimmedValue.length), attrsList);
+                        }
+                    }.0
+                }
+            }
+
+            /**
+             * Recursively find all tags in the document, and their children
+             * @param htmlString
+             * @param childList
+             * @returns {*}
+             */
+            (function getTags(htmlString, childList){
+                var startTagBeginLocation = htmlString.indexOf("<");
+                if(startTagBeginLocation >= 0 ){
+                    htmlString = htmlString.substring(startTagBeginLocation);
+                    startTagBeginLocation = 0;
+
+                    var child = new Child();
+
+                    //get tag by either finding a space or the end of the tag
+                    var startTagEndLocation = htmlString.substring(startTagBeginLocation).search(/\/?>/);
+                    var startTagEnd = htmlString.match(/\/?>/);
+                    var firstSpaceLocation = htmlString.substring(startTagBeginLocation).indexOf(" ");
+                    var tagNameEndLocation = startTagEndLocation < firstSpaceLocation ? startTagEndLocation : firstSpaceLocation;
+                    var tagName = htmlString.substring(startTagBeginLocation + 1, tagNameEndLocation);
+
+                    getTagAttributes(htmlString.substring(startTagBeginLocation, startTagEndLocation) + startTagEnd, child.attrs);
+
+                    child.name = tagName;
+
+                    if(child.name.search(/body/i) >= 0)
+                        self.html.body = child;
+                    else if(child.name.search(/head/i) >= 0)
+                        self.html.head = child;
+                    else if(child.name.search(/!doctype/i) >= 0){
+                        self.html.docType = child;
+                    }
+
+                    var endTagBeginLocation = htmlString.indexOf("</" + tagName);
+                    if(endTagBeginLocation >= 0){
+
+                        //get children of this tag
+                        getTags(htmlString.substring(startTagEndLocation, endTagBeginLocation), child.childNodes);
+
+                        child.innerHTML = htmlString.substring(startTagEndLocation + 1, endTagBeginLocation);
+                        child.outerHTML = htmlString.substring(startTagBeginLocation, endTagBeginLocation + ("/" + tagName + ">").length);
+
+                        //add children of object to the object itself
+                        childList.push(child);
+
+                        //go to next sibling
+                        getTags(htmlString.substring(endTagBeginLocation + ("</" + tagName).length), childList);
+                    }else{
+                        childList.push(child);
+                        getTags(htmlString.substring(startTagEndLocation), childList);
+                    }
+                }
+            })(htmlString, this.html.children);
+
+            return this.html;
+        },
+        html: {
+            body: {},
+            children: [],
+            docType: {
+                name: "",
+                publicId:"",
+                systemId:""
+            },
+            head: {}
+        }
+
+    }
+})(window)
